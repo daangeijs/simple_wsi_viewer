90934fa968eb74cc64d5498283f37df91094afdb
daangeijs <daan.geijs@radboudumc.nl>
2023-09-15 14:05:00 +0200

    remove models for now

--- viewer/models.py
+++ viewer/models.py
@@ -1,5 +1,5 @@
-from django.db import models
-
-class UploadedFile(models.Model):
-    uploaded_file = models.FileField(upload_to='uploads/')
-    dzi_file = models.FilePathField(blank=True, null=True)
+# from django.db import models
+#
+# class UploadedFile(models.Model):
+#     uploaded_file = models.FileField(upload_to='uploads/')
+#     dzi_file = models.FilePathField(blank=True, null=True)
